a360aa49aba25bd564639fb70fd9e41bacdba8a1
Yaret Chuica <yaretchuica10@gmail.com>
2022-11-06 09:04:29 -0500

    Tarea2 Corregir el arranque del programa

--- Tarea_2.py
+++ Tarea_2.py
@@ -50,7 +50,7 @@ def salir():
      while True:
         salir = input("\nEscriba X para salir: ")
         if salir == "x":
-            menu()
+            run()
             return 
 
 def getPokemonGeneration():
@@ -81,31 +81,36 @@ def getPokemonGeneration():
 #Se llama a la función getPokemonGeneration('generación_a_buscar')
 #getPokemonGeneration()
 
-#Nota: Aún necesita ser añadido la imagen del Pokemon a la función
+#
 
 def getPokemonSheps():
 
+
     
-    #url_pokeapi_pokemon = 'https://pokeapi.co/api/v2/pokemon/'
+    url_pokeapi_pokemon = 'https://pokeapi.co/api/v2/pokemon/'
     url_pokeapi_sheps = 'https://pokeapi.co/api/v2/pokemon-shape/'
     sub_response= requests.get(url_pokeapi_sheps)
-    data_2 = sub_response.json()
+    data_1 = sub_response.json()
     #sugerir formas---
-    lista_formas = [forma['name'] for forma in data_2['results']]
+    lista_formas = [forma['name'] for forma in data_1['results']]
     print(lista_formas)
     forma = input("Ingrese la forma del pokemon: ")
     #se hace petición al api y se convierte los datos a .json
     response = requests.get(url_pokeapi_sheps + forma)   
-    data_1 = response.json()
+    data_2 = response.json()
     #se usa comprehension para añadir elementos de los datos_1 a la lista_especies
-    lista_especies = [especies['name'] for especies in data_1['pokemon_species']]
+    lista_especies = [especies['name'] for especies in data_2['pokemon_species']]
     print('Los pokemons que se son de forma', forma, 'són: \n ')  
     #se usa el segundo api, para añadir las habilidades de cada pokemon en lista_especies
     for pokemon in lista_especies:     
-        print(pokemon)
-
-    salir()
+        sub_response= requests.get(url_pokeapi_pokemon + pokemon)
+        data_3 = sub_response.json()
+        Habilidad = [habilidad["ability"]['name'] for habilidad in data_3['abilities']]
         
+        print(pokemon)
+        print(Habilidad)
+    salir()
+  
 
       
 #getPokemonGeneration()
@@ -113,7 +118,7 @@ def getPokemonSheps():
 def getPokemonAbility():
 
     
-    #url_pokeapi_pokemon = 'https://pokeapi.co/api/v2/pokemon/'
+    url_pokeapi_pokemon = 'https://pokeapi.co/api/v2/pokemon/'
     url_pokeapi_ability = 'https://pokeapi.co/api/v2/ability/'
     sub_response= requests.get(url_pokeapi_ability)
     data_2 = sub_response.json()
@@ -128,14 +133,20 @@ def getPokemonAbility():
     lista_especies = [especies['pokemon']['name'] for especies in data_1['pokemon']]
     print('Los pokemons que se son de habilidad', Habilidad, 'són: \n ')  
     #se usa el segundo api, para añadir las habilidades de cada pokemon en lista_especies
-    for pokemon in lista_especies:     
+    for pokemon in lista_especies:  
+        sub_response= requests.get(url_pokeapi_pokemon + pokemon)
+        data_3 = sub_response.json()
+        Habilidad = [habilidad["ability"]['name'] for habilidad in data_3['abilities']]
+        
         print(pokemon)
+        print(Habilidad)   
+        
     salir()
 #getPokemonAbility()
 
 def getPokemonHabitat():
 
-    #url_pokeapi_pokemon = 'https://pokeapi.co/api/v2/pokemon/'
+    url_pokeapi_pokemon = 'https://pokeapi.co/api/v2/pokemon/'
     url_pokeapi_habitad = 'https://pokeapi.co/api/v2/pokemon-habitat/'
     sub_response= requests.get(url_pokeapi_habitad)
     data_2 = sub_response.json()
@@ -151,13 +162,19 @@ def getPokemonHabitat():
     print('Los pokemons que se son de Habitad', Habitad, 'són: \n ')  
     #se usa el segundo api, para añadir las habilidades de cada pokemon en lista_especies
     for pokemon in lista_especies:     
+        sub_response= requests.get(url_pokeapi_pokemon + pokemon)
+        data_3 = sub_response.json()
+        Habilidad = [habilidad["ability"]['name'] for habilidad in data_3['abilities']]
+        
         print(pokemon)
+        print(Habilidad)
+        
     salir()
 #getPokemonHabitat()
 
 def getPokemonType():
 
-    #url_pokeapi_pokemon = 'https://pokeapi.co/api/v2/pokemon/'
+    url_pokeapi_pokemon = 'https://pokeapi.co/api/v2/pokemon/'
     url_pokeapi_tipo = 'https://pokeapi.co/api/v2/type/'
     sub_response= requests.get(url_pokeapi_tipo)
     data_2 = sub_response.json()
@@ -172,8 +189,14 @@ def getPokemonType():
     lista_especies = [especies['pokemon']['name'] for especies in data_1['pokemon']]
     print('Los pokemons que se son de tipo', tipo, 'són: \n ')  
     #se usa el segundo api, para añadir las habilidades de cada pokemon en lista_especies
-    for pokemon in lista_especies:     
+    for pokemon in lista_especies:    
+        sub_response= requests.get(url_pokeapi_pokemon + pokemon)
+        data_3 = sub_response.json()
+        Habilidad = [habilidad["ability"]['name'] for habilidad in data_3['abilities']]
+        
         print(pokemon)
+        print(Habilidad) 
+        
     salir()
 #getPokemonType()
 
