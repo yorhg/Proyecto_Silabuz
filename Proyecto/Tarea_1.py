bfd7697d7c31bb0177adb99532a4f316eb6f04e8
Yaret Chuica <yaretchuica10@gmail.com>
2022-11-06 23:41:50 -0500

    Agregando funcionalidad

--- Proyecto/Tarea_1.py
+++ Proyecto/Tarea_1.py
@@ -32,11 +32,15 @@ def Opcion_1():
         for row in reader:
             results.append(row)
         print(results)
+    input("Presione cualquier tecla para volver al menu: ")
+    run()
 
 def Opcion_2():
     print('-----------------------LIBROS-----------------------\n')
     datos=pd.read_csv('Libros.csv', header=0)
     print(datos)
+    input("Presione cualquier tecla para volver al menu: ")
+    run()
 
 bibliotecaList = []
 def Opcion_3():
@@ -90,153 +94,181 @@ def save_books_on_csv():
      
   
 def Opcion_5():
+    go = True
+    while go == True:
+        datos = pd.read_csv('Libros.csv')
+        df = pd.DataFrame(datos)
+        
+        print('Seleccionar una opción:\n[1] Buscar por ISBN\n[2] Buscar por Titulo')
+        selección = input('Ingresar opción:\n->')
 
-    datos = pd.read_csv('Libros.csv')
-    df = pd.DataFrame(datos)
-    
-    print('Seleccionar una opción:\n[1] Buscar por ISBN\n[2] Buscar por Titulo')
-    selección = input('Ingresar opción:\n->')
+        if selección == '1':
+            Buscador_isbn = input('Escribe el ISBN del libro a Buscar:\n->')
+            respuesta = df[df['ISBN'] == Buscador_isbn]
+            print(respuesta)
+        elif selección == '2':
+            Buscador_tit = input('Escriba el titulo del libro a buscar:\n->')
+            respuesta = df[df['titulo'] == Buscador_tit]
+            print(respuesta)
+        else:
+            print('Opción inválida')
+            time.sleep(1)
+            Opcion_5()
 
-    if selección == '1':
-        Buscador_isbn = input('Escribe el ISBN del libro a Buscar:\n->')
-        respuesta = df[df['ISBN'] == Buscador_isbn]
-        print(respuesta)
-    elif selección == '2':
-        Buscador_tit = input('Escriba el titulo del libro a buscar:\n->')
-        respuesta = df[df['titulo'] == Buscador_tit]
-        print(respuesta)
-    else:
-        print('Opción inválida')
-        time.sleep(1)
-        Opcion_5()
+        repetir = input("Escriba 'si' para continuar listando o 'x' para salir al menu: ")
+        if repetir != "si":
+            go = False 
+            run()
 
 def Opcion_6():
     datos=pd.read_csv('Libros.csv', header=0)
     print(datos.sort_values(by='titulo'))
+    input("Presione cualquier tecla para volver al menu: ")
+    run()
+   
 
 
 def Opcion_7():
-
-    datos = pd.read_csv('Libros.csv')
-    df = pd.DataFrame(datos)
-    
-    print('Seleccionar una opción:\n[1] Buscar por Autor\n[2] Buscar por Editorial\n[3] Buscar por Género')
-    selección = input('Ingresar opción:\n->')
-
-    if selección == '1':
-        Buscador_isbn = input('Escribe el Autor del libro a Buscar:\n->')
-        respuesta = df[df['autor(es)'] == Buscador_isbn]
-        print(respuesta)
-    elif selección == '2':
-        Buscador_tit = input('Escriba el Editorial del libro a buscar:\n->')
-        respuesta = df[df['editorial'] == Buscador_tit]
-        print(respuesta)
-    elif selección == '3':
-        Buscador_tit = input('Escriba el Género del libro a buscar:\n->')
-        respuesta = df[df['genero'] == Buscador_tit]
-        print(respuesta)
-    else:
-        print('Opción inválida')
-        time.sleep(1)
-        Opcion_7()
-
-def Opcion_8():
-    Buscador = int(input('Ingresa la cantidad de autores:\n->'))
-    datos = pd.read_csv('Libros.csv', header=0)
-    df = pd.DataFrame(datos)
-    respuesta = df['autor(es)']
-    for linea in respuesta:
-        lista = ast.literal_eval(linea)
-        if Buscador == len(lista):
-            asd = df[df['autor(es)'] == linea]
-            print(asd)
-
-def opcion_9():
-    print(
-        'Selecciona una opción:\n',
-        '[1]Modificar titulo\n',
-        '[2]Modificar genero\n',
-        '[3]Modificar ISBN\n',
-        '[4]Modificar editorial\n',
-        '[5]Modificar autor(es)\n',
-        '[6]Volver al MENU'
-        )
-    Result = int(input('Ingresa una opción\n->'))
-    filename = "Libros.csv"
-    datos=pd.read_csv('Libros.csv', header=0)
-
-    df = pd.read_csv(filename)
-    if Result == 1:
-        df=pd.DataFrame(datos)
-        print(datos)
-        valin = input('Ingresa el dato a cambiar:\n->')
-        valout = input('Ingresa el dato que lo reemplazará:\n->')
-        df.loc[df['titulo'] == valin, "titulo"] = valout
-        df.to_csv(filename, index=False)
-        opcion_9()
-
-    elif Result == 2:
-        df=pd.DataFrame(datos)
-        print(datos)
-        valin = input('Ingresa el dato a cambiar:\n->')
-        valout = input('Ingresa el dato que lo reemplazará:\n->')
-        df.loc[df['genero'] == valin, "genero"] = valout
-        df.to_csv(filename, index=False)
-        opcion_9()
-
-    elif Result == 3:
-        df=pd.DataFrame(datos)
-        print(datos)
-        valin = input('Ingresa el dato a cambiar:\n->')
-        valout = input('Ingresa el dato que lo reemplazará:\n->')
-        df.loc[df['ISBN'] == valin, "ISBN"] = valout
-        df.to_csv(filename, index=False)
-        opcion_9()
-
-    elif Result == 4:
-        df=pd.DataFrame(datos)
-        print(datos)
-        valin = input('Ingresa el dato a cambiar:\n->')
-        valout = input('Ingresa el dato que lo reemplazará:\n->')
-        df.loc[df['editorial'] == valin, "editorial"] = valout
-        df.to_csv(filename, index=False)
-        opcion_9()
-
-    elif Result == 5:
-        df=pd.DataFrame(datos)
-        print(datos)
-        valin = input('Ingresa el dato a cambiar:\n->')
-        valout = input('Ingresa el dato que lo reemplazará:\n->')
-        df.loc[df['autor(es)'] == valin, "autor(es)"] = valout
-        df.to_csv(filename, index=False)
-        opcion_9()
-
-    elif Result == 6:
-        run()
-
-    else:
-        print('Valor ingresado incorrecto')
-        opcion_9()
-
-def opcion_4():
-    l1 = []
     go = True
     while go == True:
-        with open(r'Libros.csv', 'r') as fp:
-            l1 = fp.readlines()
-            print('-----------------------LIBROS-----------------------\n')
-            datos=pd.read_csv('Libros.csv', header=0)
-            print(datos)
-            num = int(input("Ingrese el numero enlistado del libro: "))
-        with open(r"Libros.csv", 'w') as fp:
-            for number, line in enumerate(l1):
-                if number not in [num+1]:
-                    fp.write(line)
-
-        repetir = input("Escriba 'si' para continuar borrando o 'x' para salir al menu: ")
+        datos = pd.read_csv('Libros.csv')
+        df = pd.DataFrame(datos)
+        
+        print('Seleccionar una opción:\n[1] Buscar por Autor\n[2] Buscar por Editorial\n[3] Buscar por Género')
+        selección = input('Ingresar opción:\n->')
+        
+        if selección == '1':
+            Buscador_isbn = input('Escribe el Autor del libro a Buscar:\n->')
+            respuesta = df[df['autor(es)'] == Buscador_isbn]
+            print(respuesta)
+        elif selección == '2':
+            Buscador_tit = input('Escriba el Editorial del libro a buscar:\n->')
+            respuesta = df[df['editorial'] == Buscador_tit]
+            print(respuesta)
+        elif selección == '3':
+            Buscador_tit = input('Escriba el Género del libro a buscar:\n->')
+            respuesta = df[df['genero'] == Buscador_tit]
+            print(respuesta)
+        else:
+            print('Opción inválida')
+            time.sleep(1)
+            Opcion_7()
+        repetir = input("Escriba 'si' para continuar listando o 'x' para salir al menu: ")
         if repetir != "si":
             go = False 
             run()
+        
+
+def Opcion_8():
+    go = True
+    while go == True:
+        Buscador = int(input('Ingresa la cantidad de autores:\n->'))
+        datos = pd.read_csv('Libros.csv', header=0)
+        df = pd.DataFrame(datos)
+        respuesta = df['autor(es)']
+        for linea in respuesta:
+            lista = ast.literal_eval(linea)
+            if Buscador == len(lista):
+                asd = df[df['autor(es)'] == linea]
+                print(asd)
+        repetir = input("Escriba 'si' para continuar listando o 'x' para salir al menu: ")
+        if repetir != "si":
+            go = False 
+            run()
+
+def opcion_9():
+    go = True
+    while go == True:
+        print(
+            'Selecciona una opción:\n',
+            '[1]Modificar titulo\n',
+            '[2]Modificar genero\n',
+            '[3]Modificar ISBN\n',
+            '[4]Modificar editorial\n',
+            '[5]Modificar autor(es)\n',
+            '[6]Volver al MENU'
+            )
+        Result = int(input('Ingresa una opción\n->'))
+        filename = "Libros.csv"
+        datos=pd.read_csv('Libros.csv', header=0)
+
+        df = pd.read_csv(filename)
+        if Result == 1:
+            df=pd.DataFrame(datos)
+            print(datos)
+            valin = input('Ingresa el dato a cambiar:\n->')
+            valout = input('Ingresa el dato que lo reemplazará:\n->')
+            df.loc[df['titulo'] == valin, "titulo"] = valout
+            df.to_csv(filename, index=False)
+            opcion_9()
+
+        elif Result == 2:
+            df=pd.DataFrame(datos)
+            print(datos)
+            valin = input('Ingresa el dato a cambiar:\n->')
+            valout = input('Ingresa el dato que lo reemplazará:\n->')
+            df.loc[df['genero'] == valin, "genero"] = valout
+            df.to_csv(filename, index=False)
+            opcion_9()
+
+        elif Result == 3:
+            df=pd.DataFrame(datos)
+            print(datos)
+            valin = input('Ingresa el dato a cambiar:\n->')
+            valout = input('Ingresa el dato que lo reemplazará:\n->')
+            df.loc[df['ISBN'] == valin, "ISBN"] = valout
+            df.to_csv(filename, index=False)
+            opcion_9()
+
+        elif Result == 4:
+            df=pd.DataFrame(datos)
+            print(datos)
+            valin = input('Ingresa el dato a cambiar:\n->')
+            valout = input('Ingresa el dato que lo reemplazará:\n->')
+            df.loc[df['editorial'] == valin, "editorial"] = valout
+            df.to_csv(filename, index=False)
+            opcion_9()
+
+        elif Result == 5:
+            df=pd.DataFrame(datos)
+            print(datos)
+            valin = input('Ingresa el dato a cambiar:\n->')
+            valout = input('Ingresa el dato que lo reemplazará:\n->')
+            df.loc[df['autor(es)'] == valin, "autor(es)"] = valout
+            df.to_csv(filename, index=False)
+            opcion_9()
+
+        elif Result == 6:
+            run()
+
+        else:
+            print('Valor ingresado incorrecto')
+            opcion_9()
+
+        repetir = input("Escriba 'si' para continuar Editando o 'x' para salir al menu: ")
+        if repetir != "si":
+            go = False 
+            run()
+
+def opcion_4():
+        l1 = []
+        go = True
+        while go == True:
+            with open(r'Libros.csv', 'r') as fp:
+                l1 = fp.readlines()
+                print('-----------------------LIBROS-----------------------\n')
+                datos=pd.read_csv('Libros.csv', header=0)
+                print(datos)
+                num = int(input("Ingrese el numero enlistado del libro: "))
+            with open(r"Libros.csv", 'w') as fp:
+                for number, line in enumerate(l1):
+                    if number not in [num+1]:
+                        fp.write(line)
+
+            repetir = input("Escriba 'si' para continuar borrando o 'x' para salir al menu: ")
+            if repetir != "si":
+                go = False 
+                run()
                 
 def run():
     #encabezado()
